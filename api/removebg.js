061dc2588ce97ffb752a51a79e2d6e3c6bef321a
OwnBlox <136041487+Kwkwkwjwowj@users.noreply.github.com>
2025-05-25 06:00:28 +0800

    Add files via upload

--- api/removebg.js
+++ api/removebg.js
@@ -4,46 +4,37 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   const imageUrl = req.query.url;
-
-  if (!imageUrl) return res.status(400).json({ status: false, message: 'URL gambar tidak ditemukan.' });
+  if (!imageUrl) return res.status(400).json({ status: false, message: 'Parameter url tidak ditemukan' });
 
   try {
-    const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-    const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+    const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+    const imageBuffer = Buffer.from(response.data, 'binary');
     const result = await removebg(imageBuffer);
 
-    if (!result.status) {
-      return res.status(500).json({ status: false, message: 'Gagal memproses gambar.' });
-    }
-
     const base64Data = result.image.replace(/^data:image\/png;base64,/, '');
-    const buffer = Buffer.from(base64Data, 'base64');
-    res.set('Content-Type', 'image/png');
-    res.send(buffer);
+    const imgBuffer = Buffer.from(base64Data, 'base64');
 
+    res.set('Content-Type', 'image/png');
+    res.send(imgBuffer);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ status: false, message: 'Terjadi kesalahan saat memproses permintaan.' });
+    res.status(500).json({ status: false, message: 'Gagal memproses gambar' });
   }
 });
 
 async function removebg(buffer) {
-  try {
-    if (!buffer) return { status: false, message: "Buffer tidak ditemukan" };
-    return await new Promise((resolve, reject) => {
-      const image = buffer.toString("base64");
-      axios.post("https://us-central1-ai-apps-prod.cloudfunctions.net/restorePhoto", {
-        image: `data:image/png;base64,${image}`,
-        model: "fb8af171cfa1616ddcf1242c093f9c46bcada5ad4cf6f2fbe8b81b330ec5c003"
-      }).then(res => {
-        const data = res.data?.replace(`"`, "");
-        if (!data) return reject("Gagal menghapus latar belakang gambar.");
-        resolve({ status: true, image: data });
-      }).catch(reject);
-    });
-  } catch (e) {
-    return { status: false, message: e };
-  }
+  if (!buffer) throw new Error("Buffer tidak ditemukan");
+  const image = buffer.toString("base64");
+
+  const response = await axios.post("https://us-central1-ai-apps-prod.cloudfunctions.net/restorePhoto", {
+    image: `data:image/png;base64,${image}`,
+    model: "fb8af171cfa1616ddcf1242c093f9c46bcada5ad4cf6f2fbe8b81b330ec5c003"
+  });
+
+  const data = response.data?.replace(/"/g, "");
+  if (!data) throw new Error("Gagal menghapus latar belakang gambar");
+
+  return { status: true, image: data };
 }
 
 module.exports = router;
